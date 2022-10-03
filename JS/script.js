9b27ec7209485af9b54a91192917f91813039503
Hamza-Hamli <hamzah.alhamli@gmail.com>
2022-10-03 19:04:01 +0300

    trying to make it work (API)

--- JS/script.js
+++ JS/script.js
@@ -20,12 +20,14 @@ locationBtn.addEventListener("click", () =>{
     }
 });
 function requestApi(city){
-    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=your_api_key`;
-    fetchData();
+    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid={cde3351e8c8957d723c9b16658983615
+}`;
+    fetchData(api).then(response => console.log(response.json()));
 }
 function onSuccess(position){
     const {latitude, longitude} = position.coords;
-    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=your_api_key`;
+    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=cde3351e8c8957d723c9b16658983615
+`;
     fetchData();
 }
 function onError(error){
